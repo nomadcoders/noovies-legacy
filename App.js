40083658ed413afd7cfce434160c064cc1ffc290
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-03-18 16:48:34 +0900

    #1.1 cacheImages

--- App.js
+++ App.js
@@ -1,19 +1,34 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { AppLoading } from "expo";
+import { Text, Image } from "react-native";
+import { Asset } from "expo-asset";
+
+const cacheImages = images =>
+  images.map(image => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>I love three platforms</Text>
-    </View>
+  const [isReady, setIsReady] = useState(false);
+  const loadAssets = async () => {
+    const images = cacheImages([
+      "https://images.unsplash.com/photo-1584486188544-dc2e1417aff1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      require("./assets/splash.png")
+    ]);
+    console.log(images);
+  };
+  const onFinish = () => setIsReady(true);
+  return isReady ? (
+    <Text>Ready!</Text>
+  ) : (
+    <AppLoading
+      startAsync={loadAssets}
+      onFinish={onFinish}
+      onError={console.error}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
