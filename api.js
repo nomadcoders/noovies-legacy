3224a8f014c95b1ec87f199702048bdd8c405a69
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-03-18 19:31:09 +0900

    #2.3 Bulletproof getAnything

--- api.js
+++ api.js
@@ -13,10 +13,12 @@ const makeRequest = (path, params) =>
 const getAnything = async (path, params = {}) => {
   try {
     const {
-      data: { results }
+      data: { results },
+      data
     } = await makeRequest(path, params);
-    return [results, null];
+    return [results || data, null];
   } catch (e) {
+    console.log(e);
     return [null, e];
   }
 };
--- screens/Movies.js
+++ screens/Movies.js
@@ -3,16 +3,34 @@ import { View, Text, Button } from "react-native";
 import { movieApi } from "../api";
 
 export default () => {
+  const [movies, setMovies] = useState({
+    nowPlaying: [],
+    popular: [],
+    upcoming: [],
+    nowPlayingError: null,
+    popularError: null,
+    upcomingError: null
+  });
   const getData = async () => {
-    const [nowPlaying, error] = await movieApi.nowPlaying();
+    const [nowPlaying, nowPlayingError] = await movieApi.nowPlaying();
     const [popular, popularError] = await movieApi.popular();
+    const [upcoming, upcomingError] = await movieApi.upcoming();
+    setMovies({
+      nowPlaying,
+      popular,
+      upcoming,
+      nowPlayingError,
+      popularError,
+      upcomingError
+    });
   };
   useEffect(() => {
     getData();
   }, []);
+
   return (
     <View style={{ flex: 1, backgroundColor: "black" }}>
-      <Text>Movies</Text>
+      <Text style={{ color: "white" }}>{movies.nowPlaying?.length}</Text>
     </View>
   );
 };
