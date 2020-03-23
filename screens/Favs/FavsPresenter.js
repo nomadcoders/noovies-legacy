5f203233cd1ea3d22696c7be5cb7c85f0eebc441
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-03-23 18:27:20 +0900

    #7.4 Interpolate

--- screens/Favs/FavsPresenter.js
+++ screens/Favs/FavsPresenter.js
@@ -41,16 +41,27 @@ export default ({ results }) => {
       }).start();
     }
   });
+  const roationValues = position.x.interpolate({
+    inputRange: [-100, 0, 100],
+    outputRange: ["-5deg", "0deg", "5deg"],
+    extrapolate: "clamp"
+  });
+  setInterval(() => {
+    console.log(roationValues);
+  }, 500);
   return (
     <Container>
-      {results.reverse().map((result, index) => {
+      {results.map((result, index) => {
         if (index === topIndex) {
           return (
             <Animated.View
               style={{
                 ...styles,
                 zIndex: 1,
-                transform: [...position.getTranslateTransform()]
+                transform: [
+                  { rotate: roationValues },
+                  ...position.getTranslateTransform()
+                ]
               }}
               key={result.id}
               {...panResponder.panHandlers}
@@ -62,7 +73,8 @@ export default ({ results }) => {
         return (
           <Animated.View
             style={{
-              ...styles
+              ...styles,
+              zIndex: -index
             }}
             key={result.id}
             {...panResponder.panHandlers}
