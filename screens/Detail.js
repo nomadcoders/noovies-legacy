0edfe4efa729d04a1d82da79601b0d87fe6881d4
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-03-23 15:34:08 +0900

    #6.2 Detail Styles Two

--- screens/Detail.js
+++ screens/Detail.js
@@ -8,38 +8,82 @@ import Votes from "../components/Votes";
 
 const BG = styled.Image`
   width: 100%;
-  height: ${Dimensions.get("window").height / 3}px;
+  height: 100%;
   opacity: 0.4;
   position: absolute;
 `;
 
-const Header = styled.View``;
+const Header = styled.View`
+  height: ${Dimensions.get("window").height / 3}px;
+  align-items: center;
+  justify-content: flex-end;
+`;
 
-const Container = styled.View``;
+const Container = styled.View`
+  flex-direction: row;
+  align-items: center;
+  top: 30px;
+`;
 
-const Title = styled.Text``;
+const Info = styled.View`
+  width: 50%;
+  margin-left: 40px;
+`;
 
-const Info = styled.View``;
+const Title = styled.Text`
+  color: white;
+  font-weight: 600;
+  font-size: 24px;
+  margin-bottom: 10px;
+`;
+
+const Data = styled.View`
+  margin-top: 80px;
+  padding: 0px 30px;
+`;
+
+const DataName = styled.Text`
+  color: white;
+  opacity: 0.8;
+  font-weight: 800;
+  margin-bottom: 5px;
+`;
+
+const DataValue = styled.Text`
+  color: white;
+  opacity: 0.8;
+  font-weight: 500;
+`;
 
 export default ({
   navigation,
   route: {
-    params: { id, title, backgroundImage, poster, votes }
+    params: { id, title, backgroundImage, poster, votes, overview }
   }
 }) => {
   navigation.setOptions({ title });
   return (
     <ScrollContainer loading={false}>
-      <Header>
-        <BG source={{ uri: apiImage(backgroundImage, "-") }} />
-        <Container>
-          <Poster url={poster} />
-          <Info>
-            <Title>{title}</Title>
-            <Votes votes={votes} />
-          </Info>
-        </Container>
-      </Header>
+      <>
+        <Header>
+          <BG source={{ uri: apiImage(backgroundImage, "-") }} />
+          <Container>
+            <Poster url={poster} />
+            <Info>
+              <Title>{title}</Title>
+              {votes && <Votes votes={votes} />}
+            </Info>
+          </Container>
+        </Header>
+        <Data>
+          {overview && (
+            <>
+              <DataName>Overview</DataName>
+              <DataValue>{overview}</DataValue>
+            </>
+          )}
+        </Data>
+      </>
     </ScrollContainer>
   );
 };
